6fec5df34ee0513775a9ce787ddfb1dba77394ef
Ilyas <128220841+ilyas-asmouki@users.noreply.github.com>
2025-02-27 18:40:50 +0100

    implemented LEDs and 16-pos selector

--- src/main.c
+++ src/main.c
@@ -16,16 +16,63 @@ void delay(unsigned int n)
 
 int main(void)
 {
+    // 0 -> S0 = 0, S1 = 0
+    // 1 -> S0 = 1, S1 = 0
+    // 2 -> S0 = 0, S2 = 1
+    // 3 -> S0 = 1
+
+    // selector = S3 S2 S1 S0
+
+
     SystemClock_Config();
 
-    // Enable GPIOD peripheral clock
+    // Enable GPIOB, GPIOC & GPIOD peripheral clocks
+    RCC->AHB1ENR    |= RCC_AHB1ENR_GPIOBEN;
+    RCC->AHB1ENR    |= RCC_AHB1ENR_GPIOCEN;
     RCC->AHB1ENR    |= RCC_AHB1ENR_GPIODEN;
     
-    // LED7 defined in main.h
-    gpio_config_output_pushpull(FRONT_LED);
+    // LEDs defined in main.h
+    gpio_config_output_pushpull(LED1);
+    gpio_config_output_pushpull(LED3);
+    gpio_config_output_pushpull(LED5);
+    gpio_config_output_pushpull(LED7);
+    gpio_config_output_pushpull(BODY_LED);
+
+    gpio_config_input_pd(SELECTOR0);
+    gpio_config_input_pd(SELECTOR1);
+    gpio_config_input_pd(SELECTOR2);
+    gpio_config_input_pd(SELECTOR3);
 
     while (1) {
-        delay(SystemCoreClock/16);
-        gpio_toggle(FRONT_LED);
+        uint8_t S0 = (uint8_t)gpio_read(SELECTOR0);
+        uint8_t S1 = (uint8_t)gpio_read(SELECTOR1);
+        uint8_t S2 = (uint8_t)gpio_read(SELECTOR2);
+        uint8_t S3 = (uint8_t)gpio_read(SELECTOR3);
+
+        uint8_t selector = (S3 << 3) | (S2 << 2) | (S1 << 1) | S0;
+
+        switch (selector) {
+            case 0:
+            case 1:
+                gpio_toggle(LED1);
+                break;
+            case 2:
+            case 3:
+                gpio_toggle(LED3);
+                break;
+            case 4:
+            case 5:
+                gpio_toggle(LED5);
+                break;
+            case 6:
+            case 7:
+                gpio_toggle(LED7);
+                break;
+            default:
+                gpio_toggle(BODY_LED);
+                break;
+        }
+
+        // delay(SystemCoreClock/16);
     }
 }
