8b9b9443b5796a59f9b4c8b9ca0eb716a858783c
Saad Lahlou <saad.lahlou@epfl.ch>
2025-02-27 12:04:38 +0100

    task1

---
+++ src/test.s
@@ -0,0 +1,12 @@
+	.cpu cortex-m4
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"test.c"
+	.ident	"GCC: (GNU Tools for Arm Embedded Processors 7-2017-q4-major) 7.2.1 20170904 (release) [ARM/embedded-7-branch revision 255204]"
