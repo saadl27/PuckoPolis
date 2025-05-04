2d254050a923d7d6ea878e77321e7b83bfd22058
Ilyas Asmouki <ilyas.asmouki@epfl.ch>
2025-05-04 19:55:22 +0200

    color passing operational

--- src/brain.c
+++ src/brain.c
@@ -13,31 +13,22 @@ static THD_FUNCTION(ColorDecision, arg) {
     chRegSetThreadName(__FUNCTION__);
     (void)arg;
 
-    messagebus_topic_t* color_topic;
-    color_msg_t* color_values;
-
-    MUTEX_DECL(color_topic_lock);
-    CONDVAR_DECL(color_topic_condvar);
-    
-    messagebus_topic_init(&color_topic, &color_topic_lock, &color_topic_condvar, &color_values, sizeof(color_values));
-    messagebus_advertise_topic(&bus, &color_topic, "/color");
-
-    color_topic = messagebus_find_topic_blocking(&bus, "/color");
-
-    epuck_printf("COLORDEC\n");
+    messagebus_topic_t* color_topic = messagebus_find_topic_blocking(&bus, "/color");
+    color_msg_t color_values;
 
     while (1) {
-        messagebus_topic_wait(color_topic, color_values, sizeof(color_msg_t));
+        messagebus_topic_wait(color_topic, &color_values, sizeof(color_msg_t));
         char* color_s = NULL;
-        epuck_printf("ADHKSFJKALSDJFAL\n");
-        switch (color_values->color) {
+
+        switch (color_values.color) {
             case RED_COLOR: color_s = "red"; break;
             case GREEN_COLOR: color_s = "green"; break;
             case BLUE_COLOR: color_s = "blue"; break;
             case BLACK_COLOR: color_s = "black"; break;
         }
 
-        if (!color_s) epuck_printf("Color = %s\n", color_s);
+        if (color_s) epuck_printf("Color = %s\n", color_s);
+        else epuck_printf("No color\n");
     }
 }
 
