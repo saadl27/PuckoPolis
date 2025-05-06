d3040bfe9b9b446c3d99d3420869ec8fe37ea79c
Ilyas Asmouki <ilyas.asmouki@epfl.ch>
2025-05-06 19:05:44 +0200

    created base structs for imu

---
+++ src/modules/src/imu.c
@@ -0,0 +1,14 @@
+/* C Standard Library */
+#include <stdbool.h>
+
+/* ChibiOS Library */
+#include "ch.h"
+
+/* Modules Library */
+#include "main.h"
+#include "modules/include/distance.h"
+#include "modules/include/telemetry.h"
+#include "modules/include/imu.h"
+
+/* e-puck2 main processor Library */
+#include "sensors/imu.h"
