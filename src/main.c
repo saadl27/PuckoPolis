cfb4731f687529b91e3edf8db7ec77b56173da3c
Ilyas Asmouki <ilyas.asmouki@epfl.ch>
2025-05-06 14:42:54 +0200

    A* working on dummy graphs

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 
 #include "modules/include/telemetry.h"
 #include "modules/include/distance.h"
+#include "modules/include/a_star.h"
 
 /* temporary */
 #define LOOP_PERIOD_MS 100
@@ -27,8 +28,22 @@ int main(void) {
 
 	telemetry_init();
 
+    Graph graph = {0};
+
+    a_star_init_graph(&graph, 5);
+    a_star_add_edge(&graph, 1, 2, 1);
+    a_star_add_edge(&graph, 1, 3, 3);
+    a_star_add_edge(&graph, 3, 4, 1);
+    a_star_add_edge(&graph, 4, 5, 2);
+    a_star_add_edge(&graph, 2, 5, 4);
+
     /* Infinite loop. */
     while (1) {
+        Path path = {0};
+
+        test_path(&graph, &path, 1, 5);
+        test_path(&graph, &path, 1, 4);
+        
         epuck_printf("Hello world\n");
         chThdSleepMilliseconds(LOOP_PERIOD_MS);
     }
