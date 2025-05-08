9d21ff7e69b0b7939c85f29c5c766ff1f859eabe
Ilyas Asmouki <ilyas.asmouki@epfl.ch>
2025-05-08 17:55:48 +0200

    defined city graph

--- src/main.c
+++ src/main.c
@@ -30,26 +30,45 @@ int main(void) {
 
     Graph* graph = (Graph*) malloc(sizeof(Graph));
 
-    a_star_init_graph(graph, 5);
-    a_star_add_edge(graph, 1, 2, 1);
-    a_star_add_edge(graph, 1, 3, 3);
-    a_star_add_edge(graph, 3, 4, 1);
-    a_star_add_edge(graph, 4, 5, 2);
-    a_star_add_edge(graph, 2, 5, 4);
+    a_star_init_graph(graph, 15);
+
+    a_star_add_edge(graph, 1,  5,  120);
+    a_star_add_edge(graph, 1,  3,  7);
+    a_star_add_edge(graph, 1,  2,  90);
+    a_star_add_edge(graph, 2,  6,  60);
+    a_star_add_edge(graph, 2,  4,  35);
+    a_star_add_edge(graph, 4,  10, 95);
+    a_star_add_edge(graph, 4,  9,  120);
+    a_star_add_edge(graph, 5,  6,  15);
+    a_star_add_edge(graph, 5,  7,  8);
+    a_star_add_edge(graph, 7,  8,  30);
+    a_star_add_edge(graph, 6,  8,  17);
+    a_star_add_edge(graph, 7,  13, 107);
+    a_star_add_edge(graph, 8,  9,  76);
+    a_star_add_edge(graph, 11, 13, 63);
+    a_star_add_edge(graph, 13, 14, 66);
+    a_star_add_edge(graph, 14, 15, 36);
+    a_star_add_edge(graph, 9,  10, 20);
+    a_star_add_edge(graph, 10, 12, 47);
+    a_star_add_edge(graph, 3,  12, 90);
+    a_star_add_edge(graph, 12, 15, 25);
+    a_star_add_edge(graph, 3,  15, 235);
+    a_star_add_edge(graph, 11, 14, 15);
+    a_star_add_edge(graph, 9,  11, 9);
+
 
     /* Infinite loop. */
     while (1) {
         Path* path = (Path*) malloc(sizeof(Path));
 
-        test_path(graph, path, 1, 5);
-        test_path(graph, path, 1, 4);
-        test_path(graph, path, 2, 1);
-        test_path(graph, path, 2, 4);
-        test_path(graph, path, 4, 2);
+        // test_path(graph, path, 1, 3);
+        // test_path(graph, path, 4, 10);
+        test_path(graph, path, 12, 15);
+        // test_path(graph, path, 7, 5);
+
 
         free(path);
         
-        epuck_printf("Hello world\n");
         chThdSleepMilliseconds(LOOP_PERIOD_MS);
     }
 }
--- src/modules/include/a_star.h
+++ src/modules/include/a_star.h
@@ -11,9 +11,9 @@
 
 typedef struct {
     uint8_t index;
-    uint8_t g_cost; // cost from start to this node
-    uint8_t h_cost; // heuristic cost from this node to the end
-    uint8_t f_cost; // g + h
+    uint16_t g_cost; // cost from start to this node
+    uint16_t h_cost; // heuristic cost from this node to the end
+    uint16_t f_cost; // g + h
     uint8_t parent; // idx of parent node in path
     bool visited;
 } Node;
@@ -23,22 +23,22 @@ typedef struct {
     uint8_t end;
     uint8_t path[NUM_NODES];
     uint8_t path_len;
-    uint8_t path_cost; // total weight of the path
+    uint16_t path_cost; // total weight of the path
 } Path;
 
 typedef struct {
     uint8_t num_nodes;
     uint8_t num_edges;
     uint8_t edges[NUM_NODES * MAX_EDGES_PER_NODE][2]; // each edge is a pair of node indices
-    uint8_t weights[NUM_NODES * MAX_EDGES_PER_NODE];
+    uint16_t weights[NUM_NODES * MAX_EDGES_PER_NODE];
     uint8_t adjacency[NUM_NODES][MAX_EDGES_PER_NODE];
     uint8_t adjacency_count[NUM_NODES];
 } Graph;
 
 void a_star_init_graph(Graph* graph, uint8_t num_nodes);
-void a_star_add_edge(Graph* graph, uint8_t node1, uint8_t node2, uint8_t weight);
-bool a_star_find_path(Graph* graph, Path* path, uint8_t start, uint8_t end);
-uint8_t a_star_calculate_heuristic(uint8_t node, uint8_t goal);
+void a_star_add_edge(Graph* graph, uint8_t node1, uint8_t node2, uint16_t weight);
+// bool a_star_find_path(Graph* graph, Path* path, uint8_t start, uint8_t end);
+// uint16_t a_star_calculate_heuristic(uint16_t node, uint16_t goal);
 
 //testing purposes
 void test_path(Graph* graph, Path* path, uint8_t _start, uint8_t _end);
