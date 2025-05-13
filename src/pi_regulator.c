51919f4167dd31155ee916e0b756a49459cd0d1d
Saad Lahlou <saad.lahlou@epfl.ch>
2025-05-13 20:37:39 +0200

    disabled motors

--- src/pi_regulator.c
+++ src/pi_regulator.c
@@ -85,7 +85,7 @@ static THD_FUNCTION(PiRegulator, arg) {
 }
 
 void pi_regulator_start(void) {
-	chThdCreateStatic(waPiRegulator, sizeof(waPiRegulator), NORMALPRIO, PiRegulator, NULL);
+	//chThdCreateStatic(waPiRegulator, sizeof(waPiRegulator), NORMALPRIO, PiRegulator, NULL);
 }
 
 void set_enabled_motors(bool enable) {
